caa1a7f316d9a90bf1d471884f4e1668e1ba8a3b
alondraespacialoficial-alt <alondraespacialoficial@gmail.com>
2025-11-17 06:17:28 +0000

    app avatar 70%

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Contact from './components/Contact';
 import Avatar from './components/Avatar';
+import IntegrationStatus from './components/IntegrationStatus';
 
 const App: React.FC = () => {
   return (
@@ -18,6 +19,7 @@ const App: React.FC = () => {
         </main>
       </div>
       <Avatar />
+      <IntegrationStatus />
     </div>
   );
 };
